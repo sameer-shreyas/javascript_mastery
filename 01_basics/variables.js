fec338abf5fbf321b58cc0b175244cef10c1947d
Sameer <sameershreyas13@gmail.com>
2024-04-01 15:19:38 +0530

    -learned variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const name = "Sameer"
+let roll_no = 24
+var age = 31
+country = "India"
+
+console.table([name, roll_no, age, country]);
+// name = "Aman"  const cannot be changes
+
+roll_no = 23
+age = 32
+country = "Pakistan"
+
+console.table([name, roll_no, age, country]);
+// console.table
