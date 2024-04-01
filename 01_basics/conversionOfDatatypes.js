a0ac79c0982e8e71a53dd5afdf6f136646f03a21
Sameer <sameershreyas13@gmail.com>
2024-04-01 16:16:36 +0530

    dataType conversion

---
+++ 01_basics/conversionOfDatatypes.js
@@ -0,0 +1,26 @@
+// let age = undefined
+
+// let numberAge = Number(age)
+// console.log(typeof age);
+// console.log(typeof numberAge);
+// console.log(numberAge);
+
+/*
+    "200" => 200
+    "200askdas" => NaN
+    null => 0
+    undefined => NaN
+*/
+
+let isLoggedIn = undefined;
+
+let boolenIsLoggedIn =  Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(typeof boolenIsLoggedIn);
+// console.log(boolenIsLoggedIn);
+
+let age = true
+let strAge = String(age)
+
+console.log(typeof strAge);
+console.log(strAge);
