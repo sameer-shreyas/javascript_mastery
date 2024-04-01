6a2d32454c0c1d9dcc5a0cd57cc3d0f97db70764
Sameer <sameershreyas13@gmail.com>
2024-04-01 15:38:49 +0530

    learned - datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+
+let email = "sameer.shreyas@rmail.com" // string
+let age = 2312  // number
+
+
+//number => 2 to power 53
+//bigInt for larger numbers
+//string
+//boolean => true/false
+//undefined => not defined
+//null => standalone value
+
+console.table([typeof email, typeof age, typeof undefined, typeof null])
+/*
+type of null is Object
+type of undefined is undefiend
+*/
+//object
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,7 +2,7 @@ const name = "Sameer"
 let roll_no = 24
 var age = 31
 country = "India"
-
+let hobbies;
 console.table([name, roll_no, age, country]);
 // name = "Aman"  const cannot be changes
 
@@ -10,5 +10,5 @@ roll_no = 23
 age = 32
 country = "Pakistan"
 
-console.table([name, roll_no, age, country]);
+console.table([name, roll_no, age, country, hobbies]);
 // console.table
