3571f6f7d78d786ad3c4cbc895c5444ee15c5674
Sameer <sameershreyas13@gmail.com>
2024-04-02 15:34:31 +0530

    learned comparisions

---
+++ 01_basics/Comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2<1);
+// console.log(2<=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// Dont do such comparisions
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// console.log("2" === 2); //checks datatype as well as the vvalue in it
