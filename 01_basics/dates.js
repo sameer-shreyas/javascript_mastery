19ae1950a8a47dd22efe46255b1c124896110554
Sameer <sameershreyas13@gmail.com>
2024-04-03 14:51:20 +0530

    date

---
+++ 01_basics/dates.js
@@ -0,0 +1,19 @@
+//Dates
+
+let myDate = new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreatedDate=  new Date("2024-03-12");
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimestamp = Date.now();
+// console.log(myTimestamp);
+// PerformanceObserverEntryListlog
+// console.log(Math.round(Date.now()/1000));
+console.log(myDate.getMonth() + 1);
+console.log(myDate.getDay() + 1);
