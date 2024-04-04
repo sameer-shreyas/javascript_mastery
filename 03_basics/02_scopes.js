5a5f42b202205ebd16a1cc148206db4e386cc84c
Sameer <sameershreyas13@gmail.com>
2024-04-04 15:31:02 +0530

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,4 +6,44 @@
 
 //  console.log(a);
 //  console.log(b);
- console.log(c);
+//  console.log(c);
+
+
+function one(){
+    const name = "Sameer"
+    function two(){
+        const age = 19
+        console.log(name);
+    }
+    two()
+    console.log(age);
+}
+one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
