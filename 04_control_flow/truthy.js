fdac1099f3ce59318aa7889c3649daa1f0456d57
Sameer <sameershreyas13@gmail.com>
2024-04-05 17:47:12 +0530

    loops

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,31 @@
+const userEmail =[]
+
+// if(userEmail){
+//     console.log("Got userEMail");
+// }else{
+//     console.log("have not got use email");
+// }
+
+// if(userEmail.length == 0){
+//     console.log("Array is empty");
+// }else{
+//     console.log("Array is not empty");
+// }
+
+const emptyObj = {}
+
+// if((Object.keys(emptyObj)).length == 0){
+//     console.log("Object is empty")
+// }else{
+
+// }
+
+let var1;
+
+var1 = null ?? 5
+
+// console.log(var1); // output: 5
+
+let temp = 99;
+
+temp >= 100 ? console.log("Boiling") : console.log("Not boiling");;
