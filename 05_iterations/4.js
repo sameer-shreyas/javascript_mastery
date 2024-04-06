5ccf00f4be470c6b6bd2a5285a06d5638d30130a
Sameer <sameershreyas13@gmail.com>
2024-04-06 16:02:56 +0530

    forin

---
+++ 05_iterations/4.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js: "javascript",
+    cpp: "c++",
+    py: "python"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} is shortcut for ${myObject[key]}`);
+}
+
+myarr = [1, 2, 3, 5, 6, 7]
+
+for (const key in myarr) {
+    console.log(myarr[key]);
+}
+
+//maps are not iterable
