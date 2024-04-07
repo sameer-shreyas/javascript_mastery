2346c73fcd88b3637a02a749bb550741749ae7fa
Sameer <sameershreyas13@gmail.com>
2024-04-07 16:15:59 +0530

    dom-man

--- 05_iterations/8.js
+++ 05_iterations/8.js
@@ -8,6 +8,7 @@ myNums = [1, 2, 3]
 // const Total = myNums.reduce((acc, curr_val) => (acc + curr_val), 0)
 // console.log(Total);
 
+
 const ShoppingCart = [
     {
         courseName: "javascript bootcamp",
@@ -16,7 +17,8 @@ const ShoppingCart = [
     {
         courseName: "mobile dev bootcamp",
         price: 12999
-    },{
+    },
+    {
         courseName: "DataScience bootcamp",
         price: 62999
     },
